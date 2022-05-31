4cd7218e206607843b56d4dbb0b9de94018ed512
Ajay AV <ajaywynd97@gmail.com>
2022-05-31 19:03:13 +0530

    e-kart initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipieListComponent } from './recipies/recipie-list/recipie-list.component';
+import { RecipieItemComponent } from './recipies/recipie-list/recipie-item/recipie-item.component';
+import { RecipieDetailComponent } from './recipies/recipie-detail/recipie-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    RecipieListComponent,
+    RecipieItemComponent,
+    RecipieDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
